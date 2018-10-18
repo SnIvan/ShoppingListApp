f112d4f67515e2d553cde6791b4a49435997f99d
SnIvan <ivan@devastator.local>
2018-10-18 19:05:18 -0400

    Initial Commit

---
+++ ShoppingListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingListApp
+//
+//  Created by Ivan on 2018-10-18.
+//  Copyright © 2018 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
